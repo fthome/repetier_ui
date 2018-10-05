358a7d28991ecc0d4eba5570757facbf10d1179b
Frédéric Thomé <fthome@pierron.fr>
2018-10-05 16:38:17 +0200

    IFTTT

--- set_fan.py
+++ set_fan.py
@@ -1,3 +1,18 @@
 # -*-coding:Utf-8 -*
 import sys
-print("FAN "+sys.argv[1])
+import requests
+
+key = "mb17RKiX3cj7M0R7IxdDB0o5yIYaX3BY3dBP_CDrQ60"
+url_base= "https://maker.ifttt.com/trigger/"
+
+def send_cmd(cmd):
+	print("Send " + cmd)
+	r=requests.post(url_base+cmd+"/with/key/"+key)
+	print(r.text)
+
+if sys.argv[1]=='on':
+	send_cmd("fan_on")
+	
+
+if sys.argv[1]=='off':
+	send_cmd("fan_off")
