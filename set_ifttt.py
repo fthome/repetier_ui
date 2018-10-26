fcd1c45013a2ae39f980b264af7267c91ccae658
Frédéric Thomé <fthome@pierron.fr>
2018-10-26 08:07:14 +0200

    ifttt key outside github

--- set_ifttt.py
+++ set_ifttt.py
@@ -2,12 +2,15 @@
 import sys
 import requests
 
-key = "mb17RKiX3cj7M0R7IxdDB0o5yIYaX3BY3dBP_CDrQ60"
+sys.path.insert(0,'/home/pi')
+from iftt_key import *
+#key = "xxxxx"
 url_base= "https://maker.ifttt.com/trigger/"
 
 def send_cmd(cmd):
-	print("Send " + cmd)
-	r=requests.post(url_base+cmd+"/with/key/"+key)
-	print(r.text)
+        print("Send " + cmd)
+        r=requests.post(url_base+cmd+"/with/key/"+key)
+        print(r.text)
 
 send_cmd(sys.argv[1])
+
